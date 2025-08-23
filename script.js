e097900a7b9005ba32f0ff3cf38cf2cfd69b6d68
nijo <nijo1103@gmail.com>
2025-08-23 16:20:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const keysToRemove = ["variants", "url", "uniqueSkinIds", "skinExchangePaths", "seriesId", "rulesDescription", "rarity", "promotionName", "isSerial", "isInAllExpansionPacks", "foundInPacks", "characterId", "expansionIds", "amountLang_JA", "amountLang_EN", "amountLang_CN", "amountLang_FR", "amountLang_IT", "amountLang_DE", "amountLang_ES", "amountLang_BR", "amountLang_KR", "assetUrl", "displayName", "isSpecial", "packId", "sku", "slug", "sortOrderPriority", "bgColorLeft", "bgColorRight", "cardCount", "packIds", "packSetAssetUrl", "cardType", "evolutionStage", "evolutionStageNumber", "iconUrl", "isEx", "isUltraBeast", "retreatEnergyList", "damageSymbol", "isNoDamage", "pokemonAttackId", "pokemonId", "pokemonAbilityId", "closeAt", "trainerId", "trainerTypeLabel", "id"];
+
+function removeKeys(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(item => removeKeys(item, keysToRemove));
+  } else if (typeof obj === 'object' && obj !== null) {
+    return Object.fromEntries(
+      Object.entries(obj)
+        .filter(([key]) => !keysToRemove.includes(key))
+        .map(([key, value]) => [key, removeKeys(value, keysToRemove)])
+    );
+  }
+  return obj;
+}
+
+const response1 = await fetch("https://www.pokemon-zone.com/api/game/game-data/");
+const text1 = await response1.json();
+const data1 = text1.data.cards;
+const cleanedData1 = removeKeys(data1);
+
+const response2 = await fetch("https://www.pokemon-zone.com/api/players/5811072974828985/");
+const text2 = await response2.json();
+const data2 = text2.data.cards;
+const cleanedData2 = removeKeys(data2);
