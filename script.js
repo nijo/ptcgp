ca010c5fa4bc6a16834ad93710f4ebf216e888cf
nijo <nijo1103@gmail.com>
2025-10-29 13:17:06 +0530

    Update....

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const keysToRemove = ["variants", "url", "uniqueSkinIds", "skinExchangePaths", "seriesId", "rulesDescription", "rarity", "promotionName", "isSerial", "isInAllExpansionPacks", "foundInPacks", "isPromo", "characterId", "logoAssetUrl", "iconAssetUrl", "displaySchedule", "expansionIds", "amountLang_JA", "amountLang_EN", "amountLang_CN", "amountLang_FR", "amountLang_IT", "amountLang_DE", "amountLang_ES", "amountLang_BR", "amountLang_KR", "assetUrl", "displayName", "isSpecial", "packId", "sku", "slug", "sortOrderPriority", "bgColorLeft", "bgColorRight", "cardCount", "packIds", "packSetAssetUrl", "cardType", "evolutionStage", "evolutionStageNumber", "iconUrl", "isEx", "isUltraBeast", "retreatEnergyList", "damageSymbol", "isNoDamage", "pokemonAttackId", "pokemonId", "pokemonAbilityId", "closeAt", "trainerId", "trainerTypeLabel", "id"];
+const keysToRemove = ["variants", "url", "uniqueSkinIds", "skinExchangePaths", "seriesId", "mirrorType", "rulesDescription", "rarity", "promotionName", "isSerial", "isInAllExpansionPacks", "foundInPacks", "isPromo", "characterId", "logoAssetUrl", "iconAssetUrl", "displaySchedule", "expansionIds", "amountLang_JA", "amountLang_EN", "amountLang_CN", "amountLang_FR", "amountLang_IT", "amountLang_DE", "amountLang_ES", "amountLang_BR", "amountLang_KR", "assetUrl", "displayName", "isSpecial", "packId", "sku", "slug", "sortOrderPriority", "bgColorLeft", "bgColorRight", "cardCount", "packIds", "packSetAssetUrl", "cardType", "evolutionStage", "evolutionStageNumber", "iconUrl", "isEx", "isUltraBeast", "retreatEnergyList", "damageSymbol", "isNoDamage", "pokemonAttackId", "pokemonId", "pokemonAbilityId", "closeAt", "trainerId", "trainerTypeLabel", "id"];
 
 function removeKeys(obj) {
   if (Array.isArray(obj)) {
@@ -17,11 +17,35 @@ const response1 = await fetch("https://www.pokemon-zone.com/api/game/game-data/"
 const text1 = await response1.json();
 const data1 = text1.data.cards;
 const cleanedData1 = removeKeys(data1);
+const splitObjects = cleanedData1.flatMap(obj =>
+	obj.expansionCollectionNumbers.map(item => ({
+		cardId: obj.cardId,
+		rarityName: obj.rarityName,
+		illustrationUrl: obj.illustrationUrl,
+		illustratorNames: obj.illustratorNames,
+		isPromotion: obj.isPromotion,
+		name: obj.name,
+		description: obj.description,
+		flavorText: obj.flavorText,
+		promotionCardSource: obj.promotionCardSource,
+		collectionNumber: obj.collectionNumber,
+		expansionCollectionNumbers: item,
+		pokemon: obj.pokemon,
+		trainer: obj.trainer,
+		dustCost: obj.dustCost,
+		availablePacks: obj.availablePacks,
+		expansion: obj.expansion,
+		pokedexNumber: obj.pokedexNumber,
+		mirrorTypeLabel: obj.mirrorTypeLabel
+	}))
+);
+console.log(splitObjects)
 
 const response2 = await fetch("https://www.pokemon-zone.com/api/players/5811072974828985/");
 const text2 = await response2.json();
 const data2 = text2.data.cards;
 const cleanedData2 = removeKeys(data2);
+console.log(cleanedData2)
 
 
 let data3 = {}
