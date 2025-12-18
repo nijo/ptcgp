76d7688b288fafb29bd2ec0539cd2fc95c854c75
nijo <nijo1103@gmail.com>
2025-12-18 12:36:57 +0530

    Refactor fetch calls and data processing logic

--- mini.js
+++ mini.js
@@ -1,29 +1,39 @@
-const response1 = await fetch("https://www.pokemon-zone.com/api/cards/search");
-const text1 = await response1.json();
-let data1 = [];
-text1.data.results.forEach(item => {
-	slNo = item.expansionId + "-" + item.url.split("/")[3].padStart(3, "0");
-	if(!item.expansionId.startsWith("PROMO"))
-		series = item.expansionId.split("")[0];
-	else
-		series = item.expansionId.split("-")[1];
-	
-	if(item.cardDefKey.startsWith("PK"))
-		cardType = "Pokemon"
-	else
-		cardType = "Trainer"
-	
-	data1.push({slNo, series, setid: item.expansionId, name: item.name, imgUrl: item.displayImageUrl, cardId: item.cardDefKey, cardType})
-})
+const response1 = await fetch("https://www.pokemon-zone.com/api/cards/search", {
+    
+    redirect: "follow"
+  });
+  const text1 = await response1.json();
+  let data1 = [];
+  let slNo = "";
+  let series = "";
+  let cardType = "";
+  text1.data.results.forEach(item => {
+    slNo = item.expansionId + "-" + item.url.split("/")[3].padStart(3, "0");
+    if(!item.expansionId.startsWith("PROMO"))
+        series = item.expansionId.split("")[0];
+    else
+        series = item.expansionId.split("-")[1];
+    if(item.cardDefKey.startsWith("PK"))
+      cardType = "Pokemon"
+    else
+      cardType = "Trainer"
+    let str = item.displayImageUrl.split("_");
+    let rarity = str[str.length-1].replace(".webp", "");
 
-const response2 = await fetch("https://www.pokemon-zone.com/api/players/5811072974828985");
-const text2 = await response2.json();
-let data2 = [];
-text2.data.cards.forEach(item => {
-	data2.push({cardId: item.cardId, firstReceivedAt: item.firstReceivedAt, lastReceivedAt: item. lastReceivedAt, amount: item.amount});
-})
+    data1.push({slNo, series, setId: item.expansionId, name: item.name, imgUrl: 'https://ghmaxnwhnxjjjjpnhpfi.supabase.co/storage/v1/object/public/ptcgp/Cards/' + slNo + '.webp', cardId: item.cardDefKey, cardType, rarity: rarityObj[rarity]})
+  })
 
-const combined = data1.map(d1 => {
-	const d2 = data2.find(item => item.cardId === d1.cardld);
-	return d2? {...d1, ...d2}: d1;
-});
+  const response2 = await fetch("https://www.pokemon-zone.com/api/players/5811072974828985", {
+    
+    redirect: "follow"
+  })
+  const text2 = await response2.json();
+  let data2 = [];
+  text2.data.cards.forEach(item => {
+    data2.push({cardId: item.cardId, firstReceivedAt: item.firstReceivedAt, lastReceivedAt: item.lastReceivedAt, amount: item.amount});
+  })
+
+  const combined = data1.map(meta => {
+    const inv = data2.find(item => item.cardId === meta.cardId);
+    return inv ? { ...meta, ...inv } : meta;
+  });
